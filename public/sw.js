744ded2ccd35747dca031e1fc5a10f555df1d52e
moustafafazeleabas-cpu <moustafafazeleabas@gmail.com>
2026-04-22 07:00:54 +0300

    pwa mod

---
+++ public/sw.js
@@ -0,0 +1,4 @@
+// Un Service Worker basique pour valider les critères d'installation PWA
+self.addEventListener('fetch', function(event) {
+  // On laisse le navigateur gérer les requêtes normalement
+});
